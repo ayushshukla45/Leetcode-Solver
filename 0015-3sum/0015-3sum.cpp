579989530ca9d5c8800aad7df8e3245c8e8c68e6
Ayush Shukla <urayushshukla@gmail.com>
2026-09-04 13:24:33 +0530

    Time: 39 ms (94.59%), Space: 29 MB (73.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        vector<vector<int>> ans;
+
+        vector<vector<int>> results;
         int n = nums.size();
-        
+
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n - 2; i++) {
 
-            // skip duplicate first element
-            if(i > 0 && nums[i] == nums[i-1])
+            // Skip duplicate first elements.
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
 
             int left = i + 1;
             int right = n - 1;
 
-            while(left < right) {
+            while (left < right) {
 
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum == 0) {
+                if (sum == 0) {
 
-                    ans.push_back({nums[i], nums[left], nums[right]});
+                    results.push_back(
+                        {nums[i], nums[left], nums[right]}
+                    );
 
                     left++;
                     right--;
 
-                    // skip duplicate left
-                    while(left < right && nums[left] == nums[left-1])
+                    // Skip duplicate left elements.
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
                         left++;
+                    }
 
-                    // skip duplicate right
-                    while(left < right && nums[right] == nums[right+1])
+                    // Skip duplicate right elements.
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
                         right--;
+                    }
                 }
-                else if(sum < 0) {
+                else if (sum < 0) {
                     left++;
                 }
                 else {
@@ -44,6 +51,6 @@ public:
             }
         }
 
-        return ans;
+        return results;
     }
 };
