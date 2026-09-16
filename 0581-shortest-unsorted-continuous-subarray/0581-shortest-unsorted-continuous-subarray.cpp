30468c19f0d6a9d724f5d82f2ec7d2aeedcc267e
Ayush Shukla <urayushshukla@gmail.com>
2026-09-17 00:58:33 +0530

    Time: 0 ms (100%), Space: 30.5 MB (53.48%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -6,46 +6,31 @@ public:
         int left = -1;
         int right = -1;
 
-        // Find the first position from the left
-        // where the array is decreasing
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                left = i;
-                break;
-            }
-        }   
+        int maxSeen = nums[0];
 
-        // Already sorted
-        if (left == -1) {
+        // Find the right boundary
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < maxSeen) {
+                right = i;
+            } else {
+                maxSeen = nums[i];
+            }
+        }
+
+        // If right is still -1, array is already sorted
+        if (right == -1) {
             return 0;
         }
 
-        // Find the last position from the right
-        // where the array is decreasing
-        for (int i = n - 1; i > 0; i--) {
-            if (nums[i - 1] > nums[i]) {
-                right = i;
-                break;
+        int minSeen = nums[n - 1];
+
+        // Find the left boundary
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] > minSeen) {
+                left = i;
+            } else {
+                minSeen = nums[i];
             }
-        }
-
-        // Find minimum and maximum in the unsorted part
-        int minimum = nums[left];
-        int maximum = nums[left];
-
-        for (int i = left; i <= right; i++) {
-            minimum = min(minimum, nums[i]);
-            maximum = max(maximum, nums[i]);
-        }
-
-        // Expand left if needed
-        while (left > 0 && nums[left - 1] > minimum) {
-            left--;
-        }
-
-        // Expand right if needed
-        while (right < n - 1 && nums[right + 1] < maximum) {
-            right++;
         }
 
         return right - left + 1;
